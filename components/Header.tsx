621b83f461e0cc8be77ce37977c3469ab804e663
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-12-04 22:52:32 +0100

    done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,48 +1,64 @@
-import siteMetadata from '@/data/siteMetadata'
-import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
-import Link from './Link'
-import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
-import SearchButton from './SearchButton'
+'use client'
 
-const Header = () => {
+import classNames from 'classnames'
+import headerNavLinks from 'content/headerNavLinks'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import CommandPalette from './CommandPalette/CommandPalette'
+import MobileNav from './MobileNav'
+import SectionContainer from './SectionContainer'
+import ThemeSwitch from './ThemeSwitch'
+
+export default function Header() {
+  const pathName = usePathname()
+
   return (
-    <header className="flex items-center justify-between py-10">
-      <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
-            </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
-          </div>
-        </Link>
-      </div>
-      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
+    <SectionContainer>
+      <header className="z-40 bg-transparent py-5 md:py-10">
+        <div className="mx-auto flex max-w-5xl items-center justify-between">
+          <div>
             <Link
-              key={link.title}
-              href={link.href}
-              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
+              href="/"
+              className={classNames(
+                'horizontal-underline hidden text-3xl font-extrabold sm:block',
+                {
+                  'horizontal-underline-active': pathName === '/',
+                }
+              )}
+              aria-label="d."
             >
-              {link.title}
+              d.
             </Link>
-          ))}
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
-      </div>
-    </header>
+          </div>
+          <div className="flex items-center space-x-3 text-base leading-5">
+            <div className="hidden space-x-5 sm:flex">
+              {headerNavLinks.map(({ title, href }) => {
+                const active = pathName?.includes(href)
+                return (
+                  <Link
+                    prefetch
+                    key={title}
+                    href={href}
+                    className={classNames('horizontal-underline text-base', {
+                      'horizontal-underline-active': active,
+                    })}
+                    aria-label={title}
+                  >
+                    <span className="font-semibold tracking-wide text-gray-900 dark:text-gray-100">
+                      {title}
+                    </span>
+                  </Link>
+                )
+              })}
+            </div>
+            <div className="flex items-center">
+              <CommandPalette />
+              <ThemeSwitch />
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </header>
+    </SectionContainer>
   )
 }
-
-export default Header
