6baae2d64684f42ecd0cadddb8ce8a30d3d1c566
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-12-04 19:26:24 +0100

    intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
-import Main from './Main'
 import { ScrollProvider } from '@/components/Providers/ScrollProvider'
 import Hero from '@/components/Hero'
-
+import Intro from '@/components/Intro'
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
   return (
     <ScrollProvider>
       <Hero />
+      <Intro />
     </ScrollProvider>
   )
 }
