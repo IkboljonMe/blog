1117f9dd52c26f0d12139c5badee3f9247cb6591
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-12-17 19:50:34 +0100

    Repos finished

--- components/Repos/constants.ts
+++ components/Repos/constants.ts
@@ -2,10 +2,45 @@ import { Repo } from './types'
 
 export const repos: Repo[] = [
   {
-    title: 'Aphex Planner',
-    src: 'planner-app.webp',
+    title: 'Short Url',
+    src: 'short_url.png',
     color: '#dbeafe',
-    url: 'https://app.aphex.co/',
+    url: 'https://github.com/IkboljonMe/short-url',
     role: 'Fullstack Developer',
+  },
+  {
+    title: 'Blog Template',
+    src: 'blog_template.png',
+    color: '#dbeafe',
+    url: 'https://github.com/IkboljonMe/blog-template',
+    role: 'Fullstack Developer',
+  },
+  {
+    title: 'Blogify API',
+    src: 'api.png',
+    color: '#dbeafe',
+    url: 'https://github.com/IkboljonMe/blogify-api',
+    role: 'Backend Developer',
+  },
+  {
+    title: 'My leetcode solutions',
+    src: 'leetcode.png',
+    color: '#dbeafe',
+    url: 'https://github.com/IkboljonMe/leetcode',
+    role: 'Problem Solver',
+  },
+  {
+    title: 'Minibank API',
+    src: 'api.png',
+    color: '#dbeafe',
+    url: 'https://github.com/IkboljonMe/minibank-api',
+    role: 'Backend Developer',
+  },
+  {
+    title: 'JavaScript algorithms & data structures',
+    src: 'js_algorithms.png',
+    color: '#dbeafe',
+    url: 'https://github.com/IkboljonMe/javascript-algorithms',
+    role: 'Problem Solver',
   },
 ]
